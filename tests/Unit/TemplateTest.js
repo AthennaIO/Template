d66dd97f5be23ba1a9be4b13a5da7eace1c14f2b
João Lenon <lenonSec7@gmail.com>
2022-04-29 09:38:28 -0300

    chore(tests): add lifecycle hooks in tests

--- tests/Unit/TemplateTest.js
+++ tests/Unit/TemplateTest.js
@@ -10,8 +10,13 @@
 import { test } from '@japa/runner'
 import { Bootstrap } from '#src/index'
 
-test.group('TemplateTest', () => {
+test.group('TemplateTest', (group) => {
+  group.setup(() => {})
+  group.each.setup(() => {})
+  group.teardown(() => {})
+  group.each.teardown(() => {})
+
   test('should be able to create and run tests with this template', async ({ assert }) => {
     assert.equal(Bootstrap.main('Hello', 'World!'), 'Bootstrap: Hello World!')
-  })
+  }).tags(['@template'])
 })
