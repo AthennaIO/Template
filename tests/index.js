9dd45bae7dfdb5e908b86c3975ab49d981600aab
João Lenon <lenonSec7@gmail.com>
2022-05-08 20:11:14 -0300

    feat(esm): move project to ESM

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 /**
- * @athenna/template
+ * @athenna/scaffold
  *
  * (c) João Lenon <lenon@athenna.io>
  *
@@ -7,11 +7,38 @@
  * file that was distributed with this source code.
  */
 
-import { pathToFileURL } from 'url'
+import { WelcomeServiceMock } from '#tests/Stubs/WelcomeServiceMock'
+
 import { assert } from '@japa/assert'
+import { Ignite } from '@athenna/core'
+import { Server } from '@athenna/http'
+import { pathToFileURL } from 'node:url'
 import { specReporter } from '@japa/spec-reporter'
 import { runFailedTests } from '@japa/run-failed-tests'
-import { processCliArgs, configure, run } from '@japa/runner'
+import { processCliArgs, configure, run, TestContext } from '@japa/runner'
+
+/*
+|--------------------------------------------------------------------------
+| Set test environment
+|--------------------------------------------------------------------------
+|
+| Set the test environment. This value will remove bootstrap logs and set
+| the `.env.${process.env.NODE_ENV}` file as default.
+*/
+
+process.env.NODE_ENV = 'test'
+
+/*
+|--------------------------------------------------------------------------
+| Service container mocking
+|--------------------------------------------------------------------------
+|
+| Reserve this space to set mocks for the service container using the global
+| 'ioc' instance. It's important for this code to stay before where we
+| ignite the application.
+*/
+
+ioc.mock('App/Services/WelcomeService', WelcomeServiceMock)
 
 /*
 |--------------------------------------------------------------------------
@@ -30,7 +57,38 @@ import { processCliArgs, configure, run } from '@japa/runner'
 configure({
   ...processCliArgs(process.argv.slice(2)),
   ...{
-    files: ['tests/**/*Test.js'],
+    suites: [
+      {
+        name: 'Unit',
+        files: ['tests/Unit/**/*Test.js'],
+        configure(suite) {
+          suite.setup(async () => {
+            const application = await new Ignite().fire()
+
+            TestContext.macro('request', () => {})
+            TestContext.macro('application', application)
+
+            return () => {}
+          })
+        },
+      },
+      {
+        name: 'E2E',
+        files: ['tests/E2E/**/*Test.js'],
+        configure(suite) {
+          suite.setup(async () => {
+            const application = await new Ignite().fire()
+
+            await application.bootHttpServer()
+
+            TestContext.macro('request', Server.request)
+            TestContext.macro('application', application)
+
+            return async () => await application.shutdownHttpServer()
+          })
+        },
+      },
+    ],
     plugins: [assert(), runFailedTests()],
     reporters: [specReporter()],
     importer: filePath => import(pathToFileURL(filePath).href),
