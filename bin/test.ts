1dc1c927a1c5be5fa69b689c70580e639849b53e
jlenon7 <lenonSec7@gmail.com>
2024-01-07 22:10:36 +0000

    feat(bin): update methods call

--- bin/test.ts
+++ bin/test.ts
@@ -7,11 +7,10 @@
  * file that was distributed with this source code.
  */
 
-import { Runner, assert, specReporter } from '@athenna/test'
+import { Runner } from '@athenna/test'
 
 await Runner.setTsEnv()
-  .addPlugin(assert())
-  .addReporter(specReporter())
+  .addAssertPlugin()
   .addPath('tests/unit/**/*.ts')
   .setCliArgs(process.argv.slice(2))
   .setGlobalTimeout(5000)
